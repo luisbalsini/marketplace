9a0c2e135f7b5ff941f4c902e027d19858e84678
luisbalsini <luisbalsini@hotmail.com>
2023-06-26 17:50:26 -0300

    projeto inicial estavel

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { View, Text, SafeAreaView } from 'react-native'
+
+const App = () => {
+
+
+
+    return (
+        <SafeAreaView>
+            <Text>
+                Testando
+            </Text>
+        </SafeAreaView>
+    )
+}
+
+export default App;
