002a8fc4c208f2451df63f3c6e74a2a919952879
luisbalsini <luisbalsini@hotmail.com>
2023-07-04 10:04:34 -0300

    inserindo logotipo na tela de login

--- src/modules/login/screens/login.tsx
+++ src/modules/login/screens/login.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
-import { ContainerLogin } from '../styles/login.style';
+import { ContainerLogin, ImageLogo } from '../styles/login.style';
 import Input from '../../../shared/components/input/input';
 import Button from '../../../shared/components/buttom/buttom';
 import { theme } from '../../../shared/themes/theme';
-import { Icon } from '../../../shared/components/icon/icon';
+// import { Icon } from '../../../shared/components/icon/icon';
 
 const Login = () => {
   const handleOnPress = () => {
@@ -14,7 +14,8 @@ const Login = () => {
   return (
     <View>
       <ContainerLogin>
-        <Icon name="home3" size={35} color="#000" />
+        <ImageLogo resizeMode="center" source={require('../../../assets/images/logo.png')} />
+        {/* <Icon name="home3" size={35} color="#000" /> */}
         <Input margin="0px 0px 8px 0px" placeholder="Digite seu email" title="Email" />
         <Input secureTextEntry placeholder="Digite sua senha" title="Senha" />
         <Button
--- src/modules/login/styles/login.style.ts
+++ src/modules/login/styles/login.style.ts
@@ -9,3 +9,9 @@ export const ContainerLogin = styled.View`
   align-items: center;
   padding: 16px;
 `;
+
+export const ImageLogo = styled.Image`
+  width: 150px;
+  height: 100px;
+  margin-bottom: 24px;
+`;
